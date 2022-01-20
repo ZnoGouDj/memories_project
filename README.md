c53dcc51df7af7f3d50124f214a950eee8771052
ZnoGouDj <znogoud3@gmail.com>
2022-01-20 19:17:40 +0300

    Frontend setup + Connecting to the Database

---
+++ README.md
@@ -0,0 +1,1 @@
+a simple social media MERN application that allows users to post interesting events that happened in their lives
--- server/index.js
+++ server/index.js
@@ -3,4 +3,17 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const express = require('express');
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://znogoud:12345@cluster0.jg9u5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch(error => error.message);
