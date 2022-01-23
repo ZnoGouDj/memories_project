1f91cc299a5f05e81e9581976732c71d5f78db9c
ZnoGouDj <znogoud3@gmail.com>
2022-01-23 12:34:26 +0300

    Update functionality

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
 
-function Post({ post }) {
+function Post({ post, setCurrentId }) {
   const classes = useStyles();
 
   return (
@@ -17,7 +17,13 @@ function Post({ post }) {
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{ color: 'white' }} size="small" onClick={() => {}}>
+        <Button
+          style={{ color: 'white' }}
+          size="small"
+          onClick={() => {
+            setCurrentId(post._id);
+          }}
+        >
           <MoreHorizIcon fontSize="default" />
         </Button>
       </div>
@@ -27,10 +33,10 @@ function Post({ post }) {
         </Typography>
       </div>
       <Typography className={classes.title} variant="h5" gutterBottom>
-        {post.message}
+        {post.title}
       </Typography>
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
+        <Typography variant="h5" gutterBottom>
           {post.message}
         </Typography>
       </CardContent>
